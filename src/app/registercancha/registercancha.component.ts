140ec25d734da55e37faef52c8b9237097edd2ca
Jgarcia4377 <jgarcia4377@hotmail.com>
2019-01-04 16:17:02 -0500

    notificaciones registrarEstablecimiento

--- src/app/registercancha/registercancha.component.ts
+++ src/app/registercancha/registercancha.component.ts
@@ -44,22 +44,75 @@ export class RegisterCanchaComponent implements OnInit, OnChanges, AfterViewInit
       this.establecimiento = new Establecimiento('','','','','','Ecuador','Manabí','','','','','','','','','','','','','','');
       //this.paises = new Pais('','','','','','','','','')
    }
+   showNotification(from, align,message,typeError){
+    var type = ['','info','success','warning','danger','rose','primary'];
+
+    $.notify({
+      // options
+        icon: "notifications",
+        title: "",
+        message: message,
+        url: '',
+        target: '_blank'
+      },{
+       // settings
+        element: 'body',
+        position: null,
+        type: typeError,
+        allow_dismiss: true,
+        newest_on_top: false,
+        showProgressbar: false,
+        placement: {
+          from: from,
+          align: align
+        },
+        offset: 20,
+        spacing: 10,
+        z_index: 1031,
+        delay: 3000,
+        timer: 1000,
+        url_target: '_blank',
+        mouse_over: null,
+        animate: {
+          enter: 'animated fadeInDown',
+          exit: 'animated fadeOutUp'
+        },
+        onShow: null,
+        onShown: null,
+        onClose: null,
+        onClosed: null,
+        icon_type: 'class',
+        template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+          '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+          '<span data-notify="icon"></span> ' +
+          '<span data-notify="title">{1}</span> ' +
+          '<span data-notify="message">{2}</span>' +
+          '<div class="progress" data-notify="progressbar">' +
+            '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+          '</div>' +
+          '<a href="{3}" target="{4}" data-notify="url"></a>' +
+        '</div>' 
+      });
+}
 
       onSubmit(form){
         this._establecimientoService.registrarEstablecimiento(this.establecimiento).subscribe(
           response=>{
             if(response.establecimiento && response.establecimiento._id){
-              console.log(response.establecimiento);
+              //console.log(response.establecimiento);
            // this.status ='error';
+           this.showNotification('top','center','Establecimiento Guardado Correctamente.','success'); 
               form.reset();
             }
             else{
             //this.status='success';
-            console.log(response.establecimiento);
+            this.showNotification('top','center','Complete todos los datos.','danger'); 
+            //console.log(response.establecimiento);
             form.reset();
            }
           },
           error=>{
+            this.showNotification('top','center',error.error.message,'danger');
             console.log(<any>error);
           }
         );
@@ -85,6 +138,8 @@ export class RegisterCanchaComponent implements OnInit, OnChanges, AfterViewInit
           $page.append(image_container);
       }
   };
+ 
+
 
     ngOnInit(){
         this.checkFullPageBackgroundImage();
